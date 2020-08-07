6bee0c672cd0c48aa7574f2eba37066436fceabe
Marven <57809115+MNadhum@users.noreply.github.com>
2020-08-07 07:09:51 -0400

    Can now create new and edit old pages

--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django import forms
 from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse
 
 from . import util
+
+import os
+
+
+def redirect(request):
+    return HttpResponseRedirect("/")
 
 
 def index(request):
@@ -10,17 +18,22 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def page(request, entry):
+    found = True
     information = util.get_entry(entry)
     if information == None:
+        found = False
         initial_entry = entry
         entry = "Not Found"
         information = "<h1>Entry \"" + initial_entry + "\" Does Not Exist </h1>"
-        information += "<br> Please <a href=encyclopedia:new> Create New Page </a>"
+        information += "<br>"
     return render(request, "encyclopedia/entry_page.html", {
         "entry": entry,
-        "information": information
+        "information": information,
+        "found": found
     })
+
 
 def search(request):
     query = request.GET.get("q")
@@ -39,3 +52,45 @@ def search(request):
             "query": query,
             "entries": sub_string_entries
         })
+
+
+def create(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    information = util.get_entry(title)
+    if title != None:
+        if information != None:
+            messages.error(request, "Page already exists")
+            return render(request, "encyclopedia/create.html", {})
+        else:
+            new_file = open( os.path.join("./entries" ,(str(title) + ".md")), "w")
+            new_file.write("# " + str(title) + '\n')
+            content = content.replace('\n','')
+            new_file.write(content)
+            new_file.close()
+            return HttpResponseRedirect(('/wiki/' + str(title)))
+    return render(request, "encyclopedia/create.html", {})
+
+
+def edit(request, entry):
+    content = request.GET.get("content")
+    if content != None:
+        try:
+            os.remove(os.path.join("./entries" ,(str(entry) + ".md")))
+        except:
+            pass
+        rewrite = open(os.path.join("./entries" ,(str(entry) + ".md")), "w")
+        rewrite.write(content)
+        rewrite.close()
+        return HttpResponseRedirect(('/wiki/' + str(entry)))
+    else:
+        print('else')
+        content = util.get_entry(entry)
+        if content != None:
+            return render(request, "encyclopedia/edit.html", {
+                "entry": entry,
+                "content": content
+            })
+    return render(request, "encyclopedia/index.html", {})
+
