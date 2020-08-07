26b6a23f9e82131d7ebbcfa5eea855fe99dd12cb
Marven <57809115+MNadhum@users.noreply.github.com>
2020-08-07 07:13:47 -0400

    added new url paths

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -7,7 +7,11 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.page, name="page"),
-    path("wiki/", views.index, name="no_entry"),
-    url(r'^search/$', views.search, name="search")
+    path("wiki/", views.redirect, name="no_entry"),
+    url(r'^search/$', views.search, name="search"),
+    path("create/", views.create, name="create"),
+    path("edit/<str:entry>", views.edit, name="edit"),
+    path("edit/", views.redirect, name="edit_no_entry")
+
 ]
 
